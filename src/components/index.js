61b483fe38759677d19d58d7e30fd000473e6944
Sajana Wijesinghe <svwijesinghe97@gmail.com>
2023-06-30 00:32:29 -0500

    cleanup II

--- src/components/index.js
+++ src/components/index.js
@@ -1,24 +1,29 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './Canvas';
-import Hero from './Hero/Hero';
-import Navbar from './Navbar/Navbar';
-import About from './About/About';
-import Tech from './Tech/Tech';
-import Experience from './Experience/Experience';
-import Works from './Works/Works';
-import Feedbacks from './Feedbacks/Feedbacks';
-import Contact from './Contact/Contact';
+import {
+    EarthCanvas,
+    BallCanvas,
+    ComputersCanvas,
+    StarsCanvas,
+} from "./Canvas";
+import Hero from "./Hero/Hero";
+import Navbar from "./Navbar/Navbar";
+import About from "./About/About";
+import Tech from "./Tech/Tech";
+import Experience from "./Experience/Experience";
+import Works from "./Works/Works";
+import Feedbacks from "./Feedbacks/Feedbacks";
+import Contact from "./Contact/Contact";
 
 export {
-  Hero,
-  Navbar,
-  About,
-  Tech,
-  Experience,
-  Works,
-  Feedbacks,
-  Contact,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
-  StarsCanvas
-}
+    Hero,
+    Navbar,
+    About,
+    Tech,
+    Experience,
+    Works,
+    Feedbacks,
+    Contact,
+    EarthCanvas,
+    BallCanvas,
+    ComputersCanvas,
+    StarsCanvas,
+};
